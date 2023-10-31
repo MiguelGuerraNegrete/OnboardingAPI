b2a282736bc37c0f83caec4f206f3d72e705e368
Miguel Guerra <mivue.fran2@gmail.com>
2023-10-31 16:58:03 -0500

    Agregue el proyecto de arquitectura Hexagonal

---
+++ ArquitecturaHexagonal/PruebaHexagonal2/AppTransaction.Dominio/Interfaces/IPost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTransaction.Dominio.Interfaces
+{
+    public interface IPost<Entity>
+    {
+        Entity Post(Entity entity);
+    }
+    
+}
---
+++ ArquitecturaHexagonal/PruebaHexagonal2/AppVenta.Aplication/Interfaces/IServiceMovimiento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppTransaction.Dominio;
+using AppTransaction.Dominio.Interfaces;
+
+namespace AppTransaction.Aplication.Interfaces
+{
+    public interface IServiceMovimiento<Entity, EntityId> : IPost<Entity>, IGet<Entity, EntityId>
+    {
+        void Cancel(EntityId entityId);
+    }
+}
